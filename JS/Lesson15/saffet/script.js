94ff94a9f69082f3267a271769fd839dd76f6b5f
Saffet01 <saffetr.kaban@gmail.com>
2023-06-20 00:49:30 +0300

    Saffet-Lesson15

--- JS/Lesson15/saffet/script.js
+++ JS/Lesson15/saffet/script.js
@@ -242,14 +242,173 @@ checkCategory(45);
 checkCategory(80);
 
 console.log("-------------------------------------------------");
-// 10. Write a program that takes age as a number and whether they are a student (yes or no). If the age is less than 18 and the user is a student, print "You are a student and not old enough to vote". If the age is between 18 and 65 (inclusive) and the user is not a student, print "You are eligible to vote". If the age is greater than 65, print "You are a senior citizen". If the user is a student but their age is greater than or equal to 18, print "You are old enough to vote".
+// 10. Write a program that takes age as a number and whether they are a student (yes or no). If the age is less than 18 and the user is a student, print "You are a student and not old enough to vote". 
+//If the age is between 18 and 65 (inclusive) and the user is not a student, print "You are eligible to vote". 
+//If the age is greater than 65, print "You are a senior citizen". If the user is a student but their age is greater than or equal to 18, print 
+//"You are old enough to vote".
+console.log("Ex10:");
 
-// 11. Write a program that asks the user to enter their salary and their years of experience. If the salary is less than $30,000 and the years of experience are less than 5, print "Sorry, we cannot offer you the job at this time". If the salary is less than $50,000 and the years of experience are between 5 and 10 (inclusive), print "We can offer you the job at a lower salary". If the salary is greater than or equal to $50,000 or the years of experience are greater than or equal to 10, print "We can offer you the job at the requested salary".
+function fonk(age, studentFlag){
+	if(age<18 && studentFlag ==="yes"){
+		console.log("You are a student and not old enough to vote");
+	}
+	else if(age>18 && age<65 && studentFlag==="no"){
+		console.log("You are eligible to vote");
+	}
+	else if(age>65){
+		console.log("You are a senior citizen");
+	}
+	else if(studentFlag ==="yes" && age >= 18){
+		console.log("You are old enough to vote")
+	}
+}
 
-// 12. Write a program that asks the user to enter their score on a test. If the score is less than 60, print "You failed the test". If the score is between 60 and 70 (inclusive), print "You passed the test, but your grade is not great". If the score is between 71 and 80 (inclusive), print "You got a B". If the score is between 81 and 90 (inclusive), print "You got an A". If the score is greater than 90, print "You got an A+ - Great job!".
+fonk(12, "yes");
+fonk(19,"no");
+console.log("-------------------------------------------------");
+// 11. Write a program that asks the user to enter their salary and their years of experience. 
+//If the salary is less than $30,000 and the years of experience are less than 5, print "Sorry, we cannot offer you the job at this time". 
+//If the salary is less than $50,000 and the years of experience are between 5 and 10 (inclusive), print "We can offer you the job at a lower salary". 
+//If the salary is greater than or equal to $50,000 or the years of experience are greater than or equal to 10, print "We can offer you the job at the requested salary".
+console.log("Ex11:");
 
-// 13. Write a program that asks the user to enter a month number (1-12) and displays the corresponding season name. Use a switch statement to handle the different cases.
+function interview(salary, experience){
+	if(salary<30000 && experience<5){
+		console.log("Sorry, we cannot offer you the job at this time");
+	}
+	else if(salary<50000 && experience>5 && experience<10){
+		console.log("We can offer you the job at a lower salary");
+	}
+	else if(salary>=50000 || experience>=10){
+		console.log("We can offer you the job at the requested salary");
+	}
+	else{
+		console.log("invalid expression");
+	}
+}
 
-// 14. Write a program that asks the user to enter a grade letter (A, B, C, D, or F) and displays a message indicating whether the grade is passing (A, B, C) or failing (D, F). Use a switch statement to handle the different cases.
+interview(25000, 3);
+interview(30000,7);
+interview(67000,13);
 
-// 15. Write a program that asks the user to enter their age, and then calculates and displays their age in dog years (where one human year is equivalent to seven dog years). If the user enters a negative number or zero, display an error message.
+console.log("-------------------------------------------------");
+// 12. Write a program that asks the user to enter their score on a test. If the score is less than 60, print "You failed the test". 
+//If the score is between 60 and 70 (inclusive), print "You passed the test, but your grade is not great". 
+//If the score is between 71 and 80 (inclusive), print "You got a B". If the score is between 81 and 90 (inclusive), print "You got an A". 
+//If the score is greater than 90, print "You got an A+ - Great job!".
+console.log("Ex12:");
+
+function checkScore(score){
+	if(score>0 && score<60){
+		console.log("You failed the test");
+	}
+	else if(score>60 && score<70){
+		console.log("You passed the test, but your grade is not great");
+	}
+	else if(score>71 && score<80){
+		console.log("You got a B");
+	}
+	else if(score>81 && score<90){
+		console.log("You got an A");
+	}
+	else if(score>90){
+		console.log("You got an A+ - Great job!");
+	}
+	else{
+		console.log("ınvalid input!");
+	}
+}
+
+checkScore(50);
+checkScore(70);
+checkScore(75);
+checkScore(88);
+checkScore(93);
+checkScore(-1);
+
+console.log("-------------------------------------------------");
+// 13. Write a program that asks the user to enter a month number (1-12) and displays the corresponding season name. 
+// Use a switch statement to handle the different cases.
+console.log("Ex13:");
+function findSeason(M)
+    {
+        switch (M) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Winter");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("SPRING");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("SUMMER");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("AUTUMN");
+            break;
+        default:
+            console.log("Invalid Month number");
+            break;
+        }
+    }
+findSeason(1);
+findSeason(3);
+findSeason(11);
+findSeason(8);
+
+console.log("-------------------------------------------------");
+// 14. Write a program that asks the user to enter a grade letter (A, B, C, D, or F) and displays a message indicating whether the grade is passing (A, B, C) or failing (D, F). 
+//Use a switch statement to handle the different cases.
+console.log("Ex14:");
+function findGrade(grade)
+    {
+        switch (grade) {
+        case "A":
+		case "a":
+        case "B":
+		case "b":	
+        case "C":
+		case "c":	
+            console.log("Passed");
+            break;
+        case "D":
+		case "d":
+        case "F":
+		case "f":
+            console.log("Failed");
+            break;
+        default:
+            console.log("Invalid Grade");
+            break;
+        }
+    }
+findGrade("A");
+findGrade("B");
+findGrade("F");
+findGrade("f");
+console.log("-------------------------------------------------");
+// 15. Write a program that asks the user to enter their age, and then calculates and displays their age in dog years (where one human year is equivalent to seven dog years). 
+//If the user enters a negative number or zero, display an error message.
+console.log("Ex15:");
+
+function dogYears(age2){
+	let dogYear = age2 * 7;
+	if(dogYear>0){
+		console.log(dogYear);
+	}
+	else{
+		console.log("Input can not be negative");
+	}
+}
+
+dogYears(3);
+dogYears(-3);
+
+console.log("-------------------------------------------------");
