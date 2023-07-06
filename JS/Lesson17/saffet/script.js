8e17eca39b67dca911e34a1c0b5a34ecc00ece0e
Saffet01 <saffetr.kaban@gmail.com>
2023-07-07 00:14:07 +0300

    Saffet-Lesson17

--- JS/Lesson17/saffet/script.js
+++ JS/Lesson17/saffet/script.js
@@ -1,30 +1,38 @@
 // 1. Write a function that takes an array of integers and returns the sum of all elements.
 // Input: [1, 2, 3, 4]
 // Output: 10
+console.log("Exercise 1: ");
+
 const integersEx1 = [1, 2, 3, 4];
 const integersEx1V2 = [10, 2, 30, 4];
-function sumIntegers (numbers) {
- let sum = 0;
- for (let i =0; i<numbers.length; i++) {
-  sum+= numbers[i];
- }
- return sum;
+
+function sumAllNums(intArr){
+  let sum = 0;
+  for(let i = 0; i<intArr.length; i++){
+    sum = sum + intArr[i];
+  }
+  return sum;
 }
-console.log("Exercise 1: ",sumIntegers (integersEx1) );
+
+console.log(sumAllNums(integersEx1));
+console.log(sumAllNums(integersEx1V2));
+
 console.log('----------------------------');
 // 2. Write a function that takes an array of integers and returns the average of all elements.
 //Input: [1, 2, 3, 4]
 // Output: 2.5
+console.log("Exercise 2: ");
 function getAverage (numbers) {
-  let sumArray = sumIntegers (numbers);
+  let sumArray = sumAllNums (numbers);
   return sumArray / numbers.length;
 }
-console.log("Exercise 2:", getAverage(integersEx1));
+console.log(getAverage(integersEx1));
+
 console.log('----------------------------');
 // 3. Write a function that takes an array of strings and concatenates them together adding spaces.
 // Input: ["Hello", "World", "!"]
 // Output: "Hello World !"
-
+console.log("Exercise 3: ");
 let stringE3 = ["Hello", "World", "!"];
 
 function concatString(strings){
@@ -35,29 +43,40 @@ function concatString(strings){
   return newString;
 };
 
-console.log("ex3:", concatString(stringE3));
+console.log(concatString(stringE3));
 
 console.log('----------------------------');
 // 4. Write a function that takes an array of integers and returns a new array with odd numbers only.
 // Input: [1, 2, 3, 4, 7, 10]
 // Output: [1, 3, 7]
+console.log("Exercise 4: ");
 let arrayEx = [1, 2, 3, 4, 7, 10];
 
-function getOddNumbers(numbers){
-  let newArray = [];
-  for(let i = 0 ; i< numbers.length ; i++){
-    if (!isEven(numbers[i])) newArray.push(numbers[i]);
-  } 
-  return newArray;
+// function getOddNumbers(numbers){
+//   let newArray = [];
+//   for(let i = 0 ; i< numbers.length ; i++){
+//     if (!isEven(numbers[i])) newArray.push(numbers[i]);
+//   } 
+//   return newArray;
+// }
+
+function getOddNumbers2(numberArr){
+  let oddNumsArray =[];
+  for(let i = 0; i < numberArr.length; i++){
+    if(numberArr[i]%2 === 0){
+      oddNumsArray.push(numberArr[i]);
+    }
+  }
+  return oddNumsArray;
 }
 
-console.log("Ex4", getOddNumbers(arrayEx));
-console.log("Ex4", getOddNumbers([11, 2, 3, 23, 7, 10]));
+console.log(getOddNumbers2(arrayEx));
+console.log(getOddNumbers2([11, 2, 3, 23, 7, 10]));
 console.log('----------------------------');
 // 5. Write a function that takes an array of integers and returns a new array with even numbers only.
 // Input: [1, 2, 3, 4, 7, 10]
 // Output: [1, 2, 4, 10]
-
+console.log("Exercise 5: ");
 function isEven(number){
   return number % 2 === 0;
 }
@@ -71,14 +90,15 @@ function selectIfEven(numbers){
   return evenArray;
 };
 
-console.log('Exercise 5.1: ', selectIfEven([1, 2, 3, 4, 7, 10]));
-console.log('Exercise 5.2: ', selectIfEven([1, 32, 38, 4, 74, 10]));
+console.log(selectIfEven([1, 2, 3, 4, 7, 10]));
+console.log(selectIfEven([1, 32, 38, 4, 74, 10]));
 
 
 console.log('----------------------------');
 // 6. Write a function that takes an array and a value, and returns a new array with all occurrences of the value removed.
 // Input: ([1, 2, 3, 4, 2], 2)
 // Output: [1, 3, 4]
+console.log("Exercise 6: ");
 function removeNumbers(arrayOfNumber, numberToRemove) {
   let newArray2 = [];
 
@@ -89,7 +109,7 @@ function removeNumbers(arrayOfNumber, numberToRemove) {
   }
   return newArray2;
 };
-console.log("exercise 6:", removeNumbers([1, 2, 3, 4, 2], 2));
+console.log(removeNumbers([1, 2, 3, 4, 2], 2));
 console.log('----------------------------');
 // 7. Write a function that takes an object and returns the keys as an array.
 // Input: {name: "John", age: 30, city: "New York"}
@@ -133,21 +153,80 @@ console.log('----------------------------');
 // 10. Write a function that takes an object and a key-value pair, and adds the key-value pair to the object.
 // Input: ({name: "John", age: 30}, "city", "New York")
 // Output: {name: "John", age: 30, city: "New York"}
+console.log("Exercise 10: ");
+
+function addKeyValue(object, key, value){
+  if (typeof object !== "object") {
+    console.log("Provided value is not an object");
+  }
+  else{
+    if(key in object){
+      console.log("The key is already exist");
+    }
+    else{
+    object[key] = value;
+    }
+  }
+  return object;
+}
+
+console.log(addKeyValue({name: "John", age: 30}, "city", "New York"));
+console.log(addKeyValue({name: "John", age: 30, city: "New York"}, "city", "New York"));
+
 
 console.log('----------------------------');
 // 11. Write a function that takes an object and a key, and removes the key-value pair from the object.
 // Input: ({name: "John", age: 30, city: "New York"}, "city")
 // Output: {name: "John", age: 30}
+console.log("Exercise 11: ");
+
+function removeKeyValue(object, key, value){
+  if (typeof object !== "object") {
+    console.log("Provided value is not an object");
+  }
+  else{
+    if(object[key] === undefined){
+      console.log("The key is already doesn't exist");
+    }
+    else{
+    delete object[key];
+    }
+  }
+  return object;
+}
+
+console.log(removeKeyValue({name: "John", age: 30, city: "New York"}, "city"));
+console.log(removeKeyValue({name: "John", age: 30}, "city"));
 
 console.log('----------------------------');
 // 12. Write a function that takes two arrays of equal length and returns an object with the first array as keys and the second array as values.
 // Input: (["name", "age", "city"], ["John", 30, "New York"])
 // Output: {name: "John", age: 30, city: "New York"}
+console.log("Exercise 12: ");
 
+function createObjFromTwoArray(arr1, arr2){
+  let newObj =[];
+  if(arr1.length !== arr2.length){
+    console.log("These two arrays don't have the same lenght!");
+  }
+  else{
+    for(let i = 0; i<arr1.length; i++){
+      newObj[arr1[i]] = arr2[i];
+    }
+  }
+  return newObj;
+}
+
+console.log(createObjFromTwoArray(["name", "age", "city"], ["John", 30, "New York"]));
+
+console.log('----------------------------');
 // 13. Array Destructuring
 // Given the following array, use array destructuring to assign the values of x, y, and z to their respective variables: 
+console.log("Exercise 13: ");
+
 const coordinates = [10, 20, 30];
 
+console.log('----------------------------');
 // 14. Object Destructuring
 // Given the following object, use object destructuring to assign the values of name and age to their respective variables:
 const personE14 = {
