d39fe845eed073d271e5a0ba1bd1f2137329eba1
@OnderMartin <ondermartin@gmail.com>
2023-08-04 13:16:30 +0300

    initial commit

--- JS/Lesson16/onder/script.js
+++ JS/Lesson16/onder/script.js
@@ -60,83 +60,137 @@ console.log(newArray);
 
 console.log('- - - - - - - - - - - - - - - - - ');
 // 7. Write a while loop that counts from 10 to 1 and prints out each number.
-let i = 10;
-while(i>=1) {
-	console.log(i);
-	i--;
-}
-// while (i < 10) {
-//   text += "The number is " + i;
-//   i++;
-// }
+let number7= 10;
+while(number7>=1) {
+	console.log("Exercise 7: ", number7);
+	number7--;
+ }
 
 console.log('- - - - - - - - - - - - - - - - - ');
+
 // 8. Write a while loop that generates random numbers between 1 and 10 until a number greater than 8 is generated, and then prints out the total number of iterations.
-function getRandomNumber () {
-return Math.floor(Math.random()*10)+1;
-} 
 
-let iteration = 1;
-let randomNumber = getRandomNumber();
-let randomNumbers = [randomNumber];
+let totalIterationsNumber = 0;
+let randomNumbers = 0 [randomNumber];
 
-while(randomNumber <= 8){
-randomNumber = getRandomNumber();
-randomNumbers.push(randomNumber);
-iteration++;
+while (randomNumber <= 8) {
+	randomNumber = Math.floor(Math.random() * 10) + 1;
+	totalIterationsNumber++;
 }
+console.log("Exercise 8: ", 'Total number of iterations: ', totalIterationsNumber);
 
-console.log('Iterations:', iteration, 'random number: ', randomNumber, 'Numbers: ', randomNumbers);
 
-console.log('- - - - - - - - - - - - - - - - - ');
+console.log("- - - - - - - - - - - - - - - - - ");
 // 9. Write a do-while loop that counts from 1 to 10 and prints out each number.
-let number = 1;
+let number9 = 1;
 do {
-	console.log(number);
-	number++;
-} while (number<=10);
+	console.log(number9);
+	number9++;
+} while (number9 <= 10);
 
 console.log('- - - - - - - - - - - - - - - - - ');
 // 10. Write a for...in loop that prints out the keys and values of an object.
-const person = {name: "John", surname: "Doe", age: 30, city: "New York"};
-// Array olsaydi boyle olurdu: ["John", "Doe", 30 , "New York"];
-// Array element position numbers: 0, 1, 2, 3
-// Object keys: name, surname, age, city 
+let person = {
+    name: "John",
+    surname: "Doe"
+    age: 30,
+    city: "New York"
+  };
+  
+  for (let key in person) {
+    console.log(`Key: ${key}, Value: ${person[key]}`);
+  }
+  
+console.log('- - - - - - - - - - - - - - - - - ');
 
-for (const key in person){
-	console.log(`${key}: ${person[key]}`);
-}
-console.log('- - - - - - - - - - - - - - - - - ');
 // 11. Write a for...in loop that sums the values of an object.
-const obj = {a: 10, b: 20, c: 30, d: 23};
-let sum = 0;
-for(const key in obj) {
-	sum+= obj[key];
-}
-console.log(sum);
+
+const myObject = {
+    value1: 10,
+    value2: 20,
+    value3: 30
+  };
+  
+  let sum = 0;
+  
+  for (let key in myObject) {
+    sum += myObject[key];
+  }
+  
+  console.log("Sum:", sum);
+  
 console.log('- - - - - - - - - - - - - - - - - ');
+
 // 12. Write a loop that finds the average of an array of numbers.
-const numbers12 = [10, 20, 30, 40, 50];
+
+const numbers = [10, 20, 30, 40, 50];
+let sumArray = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+const average = sum / numbers.length;
+console.log("Average:", averageArray);
+
 
 console.log('- - - - - - - - - - - - - - - - - ');
 // 13. Write a loop that prints out the sum of all even numbers between 1 and 50.
 
+let sum = 0;
+
+for (let i = 2; i <= 50; i += 2) {
+  sum += i;
+}
+
+console.log("Sum of even numbers:", sum);
 
 
 console.log('- - - - - - - - - - - - - - - - - ');
 // 14. Write a loop that finds the second largest number in an array of numbers.
 const numbers14 = [10, 5, 20, 8, 15];
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > largest) {
+    secondLargest = largest;
+    largest = numbers[i];
+  } else if (numbers[i] > secondLargest && numbers[i] < largest) {
+    secondLargest = numbers[i];
+  }
+}
+console.log("Second largest number:");
 
 console.log('- - - - - - - - - - - - - - - - - ');
+
 // 15. Write a loop that counts the number of vowels in a given string.
 const string15 = 'Hello, how are you?';
 const vowels = 'aeiouAEIOU';
+let vowelCount = 0;
+
+for (let i = 0; i < givenString.length; i++) {
+  const char = givenString[i].toLowerCase();
+  if (vowels.includes(char)) {
+    vowelCount++;
+  }
+}
+
+console.log("Number of vowels:", vowelCount sitringis15);
 
 console.log('- - - - - - - - - - - - - - - - - ');
+
 // 16. Write a loop that calculates the sum of all digits in a given number.
 function sumDigits(num) {
-  // Write your code here
+	let sum = 0;
+	let numString = num.toString();
+	for (let i = 0; i < numString.length; i++) {
+		sum += parseInt(numString[i]);
+	}
+	return sum;
+console.log("Sum of digits:", sum);
 }
+
 console.log(sumDigits(12345)); // Output: 15
 console.log('- - - - - - - - - - - - - - - - - ');
 
