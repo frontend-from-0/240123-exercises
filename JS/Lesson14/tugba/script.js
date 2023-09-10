2fca227c7ca8f50eabde7a32b773ea3187b76f78
@TugbaMartin <tugba.martin01@gmail.com>
2023-09-10 12:41:37 +0300

    initial commit

--- JS/Lesson14/tugba/script.js
+++ JS/Lesson14/tugba/script.js
@@ -1,4 +1,4 @@
-// From Lesson13: generate random number in a range from 0 to 10
+  // From Lesson13: generate random number in a range from 0 to 10
 // BONUS: generate random number in a range from 0 to 10
 // Use Math.floor() and Math.random() to do that.
 // Math.random() generate random number between 0 and 1 (not including 1)
@@ -11,21 +11,28 @@ console.log("Random number:", randomNumber);
 // 1. Use the `length` property to find the length of a string and print it to the console.
 const stringExercise1 = 'Object-oriented programming is a powerful paradigm.';
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const lengthOfString = stringExercise1.length;
 
 console.log("Length of the string:", lengthOfString);
 =======
+=======
+>>>>>>> Stashed changes
 console.log("Exercise1: ", stringExercise1.length);
 console.log('------------------------');
 
 console.log('Object-oriented programming is'.length);
 console.log('------------------------');
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
 
 console.log('------------------------');
 // 2. Use the `slice` method to extract 'Debugging code' from the string below and print it to the console.
 const stringExercise2 = 'Debugging code can be a frustrating but necessary task.';
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 const extractedPhrase = stringExercise2.slice(0, 14);
 
@@ -35,9 +42,15 @@ const sliceMethod = stringExercise2.slice(0, 14);
 console.log("Exercise2: ", sliceMethod);
 
 >>>>>>> Stashed changes
+=======
+const sliceMethod = stringExercise2.slice(0, 14);
+console.log("Exercise2: ", sliceMethod);
+
+>>>>>>> Stashed changes
 
 // 3. Use the `toUpperCase` method to convert a string to uppercase and print it to the console.
 const stringExercise3 = 'Python is a popular language for data analysis and machine learning.';
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 const uppercaseString = stringExercise3.toUpperCase();
 
@@ -56,6 +69,8 @@ const indexOfL = stringExercise5.indexOf('l');
 
 console.log("Index of 'l':", indexOfL);
 =======
+=======
+>>>>>>> Stashed changes
 const stringToUpperCase = stringExercise3.toUpperCase();
 console.log("Exercise3: ", stringToUpperCase);
 console.log('------------------------');
@@ -69,6 +84,9 @@ const stringExercise5 = 'JavaScript is a versatile language used for web develop
 const letterİndexOf = stringExercise5.indexOf("l");
 console.log("Exercise5.a: ", letterİndexOf);
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
 // 5.b Use the `indexOf` method to find the index of "language" in the string stringExercise5 and print it to the console.
@@ -79,10 +97,15 @@ console.log("Index of 'language':", indexOfLanguage);
 
 // 6. Use the `lastIndexOf` method to find the last index of of "l" character in the string stringExercise5 and print it to the console.
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const stringExercise6 = 'JavaScript is a versatile language used for web development and beyond.';
 const lastIndexOfL = stringExercise5.lastIndexOf('l');
 
 console.log("Last index of 'l':", lastIndexOfL);
+=======
+const letterLastİndexOf = stringExercise5.lastIndexOf("l");
+console.log("Exercise6: ", letterLastİndexOf);
+>>>>>>> Stashed changes
 =======
 const letterLastİndexOf = stringExercise5.lastIndexOf("l");
 console.log("Exercise6: ", letterLastİndexOf);
@@ -102,6 +125,7 @@ console.log("Last character:", lastCharacter);
 
 // 8. Use the `split` method to split a string into an array of substrings and print it to the console.
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const stringToSplit = 'Splitting a string into an array of substrings.';
 const arrayOfSubstrings = stringToSplit.split(' ');
 
@@ -120,6 +144,8 @@ const replacedString = stringExercise10.replace("can help", "help");
 console.log("Replaced string:", replacedString);
 
 =======
+=======
+>>>>>>> Stashed changes
 const stringExercise8 = 'Algorithms are a fundamental part of computer science.';
 const stringSplit = stringExercise8.split(" ");
 console.log("Exercise8: ", stringSplit);
@@ -140,6 +166,7 @@ const string1Exercise11 = "$9.99 - Sale! 50% off all items in store";
 const string2Exercise11 = "Bundle deal: Save $50 when you buy two products together";
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const startsWithPrice1 = string1Exercise11.startsWith("$");
 const startsWithPrice2 = string2Exercise11.startsWith("$");
 
@@ -152,6 +179,19 @@ const string2Exercise12 = "Bundle deal: Save $50 when you buy two products toget
 
 const endsWithExclamation1 = string1Exercise12.endsWith("!");
 const endsWithExclamation2 = string2Exercise12.endsWith("!");
+=======
+const string1Price = string1Exercise11.startsWith("$");
+console.log("Exercise11.1: ", string1Price);
+const string2Price = string2Exercise11.startsWith("$");
+console.log("Exercise11.2: ", string2Price);
+console.log('------------------------');
+// 12. Use the `endsWith` method to check if a string ends with a "!" (exclamation mark), and print the result to the console.
+const string1Exercise12 = "$9.99 - Sale! 50% off all items in store";
+const string2Exercise12 = "Bundle deal: Save $50 when you buy two products together!";
+const string1Signal = string1Exercise12.endsWith("!");
+const string2Signal = string2Exercise12.endsWith("!");
+console.log("Exercise12.2: ", string2Signal);
+>>>>>>> Stashed changes
 
 console.log("String 1 ends with exclamation mark:", endsWithExclamation1);
 console.log("String 2 ends with exclamation mark:", endsWithExclamation2);
@@ -186,9 +226,13 @@ console.log('------------------------');
 // Question: What is the difference between `substring()` and `slice()`?
 const stringExercise14 = 'Databases are an essential component of many applications.';
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const extractedWord = stringExercise14.substring(0, 9);
 
 console.log("Extracted word:", extractedWord);
+=======
+const stringSub = stringExercise14.substr
+>>>>>>> Stashed changes
 =======
 const stringSub = stringExercise14.substr
 >>>>>>> Stashed changes
@@ -196,10 +240,16 @@ const stringSub = stringExercise14.substr
 // 15. Remove extra spaces from the string stringExercise15 and print the result to the console.
 const stringExercise15 = "Hello world";
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const trimmedString = stringExercise15.trim().replace(/\s+/g, ' ');
 console.log("Exercise15: ", stringExercise15)
 
 console.log("Trimmed string:", trimmedString);
+=======
+const stringSpaceDelete = stringExercise15.trim();
+console.log("Exercise15: ", stringSpaceDelete);
+
+>>>>>>> Stashed changes
 =======
 const stringSpaceDelete = stringExercise15.trim();
 console.log("Exercise15: ", stringSpaceDelete);
@@ -215,5 +265,9 @@ console.log("Extracted price and currency:", extractedPriceCurrency);
 =======
 const planPriceString = "Premium plan - 9.99 USD/month."; // Should return "9.99 USD"
 const removeWords = planPriceString.slice(15, 23);
+<<<<<<< Updated upstream
+console.log("Exercise16: ", removeWords);
+>>>>>>> Stashed changes
+=======
 console.log("Exercise16: ", removeWords);
 >>>>>>> Stashed changes
