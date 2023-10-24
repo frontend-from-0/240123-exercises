63c6b98b72309a2e7d4af5a1b61b6b82d5af93f0
Anna S. A <anna.atalay@minna.tech>
2023-10-24 21:24:32 +0200

    Add missing files

---
+++ JS/Lesson16/savas/script.js
@@ -0,0 +1,75 @@
+// 1. Write a loop that prints out the numbers from 1 to 10.
+for (let i = 1; i<=10; i++) {
+	console.log(i);
+}
+console.log('- - - - - - - - - - - - - - - - - ');
+// 2. Write a loop that prints out the odd numbers from 1 to 20.
+// 1,3,5,7...
+for (let i = 1; i<20; i = i+2){
+	console.log(i);
+}
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 3. Write a loop that prints out the even numbers from 1 to 20.
+// 2, 4, 6...
+for (let i =2; i<=20; i+=2) {
+	console.log(i);
+}
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 4. Write a loop that finds the maximum number in an array of numbers.
+const findMaxNumberInArray = [300, 10, 23, 42, 505, 12, 7, 4, 900];
+
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 5. Write a loop that finds the minimum number in an array of numbers.
+const findMinNumberInArray = [1, 10, 23, 42, 0, 12, 7, 0];
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 6. Write a loop that reverses a given string.
+const string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 7. Write a while loop that counts from 10 to 1 and prints out each number.
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 8. Write a while loop that generates random numbers between 1 and 10 until a number greater than 8 is generated, and then prints out the total number of iterations.
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 9. Write a do-while loop that counts from 1 to 10 and prints out each number.
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 10. Write a for...in loop that prints out the keys and values of an object.
+const person = {name: "John", surname: "Doe", age: 30, city: "New York"};
+// Array olsaydi boyle olurdu: ["John", "Doe", 30 , "New York"];
+// Array element position numbers: 0, 1, 2, 3
+// Object keys: name, surname, age, city 
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 11. Write a for...in loop that sums the values of an object.
+const obj = {a: 10, b: 20, c: 30, d: 23};
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 12. Write a loop that finds the average of an array of numbers.
+const numbers12 = [10, 20, 30, 40, 50];
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 13. Write a loop that prints out the sum of all even numbers between 1 and 50.
+
+
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 14. Write a loop that finds the second largest number in an array of numbers.
+const numbers14 = [10, 5, 20, 8, 15];
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 15. Write a loop that counts the number of vowels in a given string.
+const string15 = 'Hello, how are you?';
+const vowels = 'aeiouAEIOU';
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 16. Write a loop that calculates the sum of all digits in a given number.
+
+console.log('- - - - - - - - - - - - - - - - - ');
+
