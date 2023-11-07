677be98ffc90938365ed804fda2b3798ab784499
Onur Yemez <123839486+Onuryemez54@users.noreply.github.com>
2023-11-07 03:16:45 +0300

    adding-alert-text

--- JS/Lesson31/onur/my-app/src/components/NewToDoItem/index.jsx
+++ JS/Lesson31/onur/my-app/src/components/NewToDoItem/index.jsx
@@ -13,13 +13,25 @@ export const NewToDoItem = ({ handleCreateTodo }) => {
         if (newItem.length > 0) {
             handleCreateTodo(newItem);
             setNewItem('');
+        } else {
+            alertText();
         }
         e.preventDefault();
+    }
+
+    const [alert, setAlert] = useState(false)
+
+    const alertText = () => {
+        setAlert(true);
+        setTimeout(() => {
+            setAlert(false);
+        }, 1000);
     }
 
     return (
         <>
             <h2>Create a new todo</h2>
+            {alert && <p className="alert-text">Please enter a todo</p>}
             <form onClick={handleSubmit} className="form-container" >
                 <input className="form-input" type="text" onChange={handleInput} value={newItem} />
                 <button className="form-btn" type="submit" >Add</button>
