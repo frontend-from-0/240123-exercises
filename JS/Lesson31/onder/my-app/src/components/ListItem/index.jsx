d989a3eab1e99693f24f82424cbffd89e260d1f3
onder martin <ondermartin@gmail.com>
2023-11-23 09:22:04 +0300

    completed

--- JS/Lesson31/onder/my-app/src/components/ListItem/index.jsx
+++ JS/Lesson31/onder/my-app/src/components/ListItem/index.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react';
 
 
-export const ListItem = ({ id, title, handleDelete }) => {
+export const ListItem = ({ id, title, handleDelete, completed , handleToggleComplete }) => {
   const [completed, setCompleted] = useState(false);
 
   const handleClick = () => {
     setCompleted(!completed);
+    handleToggleComplete(id);
   }
+  
+  const handleDeleteClick = () => {
+    handleDelete(id);
+  };
 
   const className = completed ? 'completed' : '';
 
   return (
-    <li onClick={handleClick}>
-      <span className={className}>{title}</span>
-      <button onClick={() => handleDelete(id)}>Delete Button</button>
+    <li onClick={completed ? 'completed' : ''}>
+      <span className={handleClick}>{title}</span>
+      <button onClick={handleDeleteClick}>Delete Button</button>
     </li>
   );
 }
