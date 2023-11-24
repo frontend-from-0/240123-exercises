3723379ab80e1d35afc084bbcefda6588e97dcfb
onder martin <ondermartin@gmail.com>
2023-11-24 12:00:15 +0300

    Onder-Lesson32-JS

--- JS/Lesson32/onder/my-app/src/App.jsx
+++ JS/Lesson32/onder/my-app/src/App.jsx
@@ -4,21 +4,29 @@ import {BD_SEARCH_BASE_URL} from './urls.js';
 import {RecipeList} from './components/RecipeList';
 import {SearchBar} from './components/SearchBar';
 
-export const App = () => {
-  // 1. Fetch recipes data from an API (Get request, API key, useState to store data, useEffect)
-  // 2. Display the data (RecipeList component -> use <ul> to display data)
-  // 3. Search recipe (A new component SearchBar with a <form> element, text input, and submit functionality)
 
-	const [recipes, setRecipes] = useState([]);
+export const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
     // If you want to keep both initial recipes and recipes from search  on the screen at the same time use code below:
   // const [searchResult, setSearchResult] = useState([]);
 
   // Runs once of component render (component render = "sayfanin yenilenmesi")
   useEffect(() => {
     fetch(BD_SEARCH_BASE_URL)
-    .then(response => response.json())
-    .then(data => setRecipes(data.meals));
-  },[]);
+      .then(response => response.json())
+      .then(data => setRecipes(data.meals));
+  }, []);
+
+  const handleRecipeClick = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleClearSelection = () => {
+      setSelectedRecipe(null);
+  };
+
 
   // If you want to keep both initial recipes and recipes from search  on the screen at the same time use code below:
   // useEffect(() => {
@@ -31,9 +39,14 @@ export const App = () => {
   
 	return (
 		<div className='container'>
-      <SearchBar setRecipes={setRecipes}/>
-      <RecipeList recipes={recipes} />
-		</div>
+      <SearchBar setRecipes={setRecipes} />
+      {/* Conditionally render RecipeDetail if a recipe is selected, otherwise render RecipeList */}
+      {selectedRecipe ? (
+        <RecipeDetail recipe={selectedRecipe} onClearSelection={handleClearSelection} />
+      ) : (
+        <RecipeList recipes={recipes} onRecipeClick={handleRecipeClick} />
+      )}
+    </div>
 	);
 };
 
