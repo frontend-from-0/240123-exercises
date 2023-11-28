b925e57bb868fb417177ea9ec535f3524151142a
onder martin <ondermartin@gmail.com>
2023-11-28 18:37:12 +0300

    corrected

---
+++ JS/Lesson32/onder/my-app/src/components/RecipeDetail/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const RecipeList = ({ recipes, onRecipeClick }) => {
+  return (
+    <div>
+      <h2>Search Results</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id} onClick={() => onRecipeClick(recipe)}>
+            {recipe.strMeal}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
