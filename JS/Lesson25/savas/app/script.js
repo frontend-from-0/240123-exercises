fb04ca82185ab7e0346d92ea10a7c520d2fb40a6
Savas Pamuk <savaspamuk@gmail.com>
2023-11-28 16:14:15 +0100

    SAVAS-Code was added to clear screen.

--- JS/Lesson25/savas/app/script.js
+++ JS/Lesson25/savas/app/script.js
@@ -14,6 +14,9 @@ const button = document.getElementById('fetch-button');
 
 function fetchImages() {
   // TODO: Clear screen before loading new images
+  
+  container.innerHTML = "";
+  
   fetch(image_url)
   .then(response => response.json())
   .then(data => {
