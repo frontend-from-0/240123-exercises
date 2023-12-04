19f96f69138a3ad5e91e7618104fc117326a1fe6
TugbaMartin <121744112+TugbaMartin@users.noreply.github.com>
2023-12-05 00:21:18 +0300

    Tugba JS-LESSON 30 (#109)
    * lesson30 done
    
    * correctıon made on lesson 30
    
    * correcten on Lesson 30
    
    * corrected lesson30
    
    * corrected lesson 30
    
    * corrected lesson 30
    
    ---------
    
    Co-authored-by: @TugbaMartin <“tugba.martin01@gmail.com”>

---
+++ JS/Lesson30/tugba/my-app/src/components/Ex8/index.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import './index.css'
+export const Ex8 = () => {
+  const initialList = [
+    { id: 1, label: "Working hard" },
+    { id: 2, label: "Go to work" },
+    { id: 3, label: "Do dishes" },
+    { id: 4, label: "Attend meetings" },
+    { id: 5, label: "Being lazy" },
+    { id: 6, label: "Being happy" },
+    { id: 7, label: "Cook meal" },
+  ];
+
+  const [list, setList] = useState(initialList);
+
+  const deleteButton = (itemId) => {
+    const updatedList = list.filter((item) => item.id !== itemId);
+    console.log(itemId);
+    console.log(updatedList);
+    return setList(updatedList);
+  };
+
+  return (
+    <>
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>
+            <span>{item.label}</span>
+            <button onClick={() => deleteButton(item.id)}>handleDeleteClick</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+/* corrected */
