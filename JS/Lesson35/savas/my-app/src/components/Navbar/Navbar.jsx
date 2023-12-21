506aefcc89571e8947ba5864bb50f5b2ab8318bd
Savas Pamuk <savaspamuk@gmail.com>
2023-12-21 13:49:22 +0100

    SAVAS-Some corrections and stylings

--- JS/Lesson35/savas/my-app/src/components/Navbar/Navbar.jsx
+++ JS/Lesson35/savas/my-app/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { NavLink, useMatch, useResolvedPath } from "react-router-dom";
 import "./navbar.css";
+
 const CustomLink = ({ to, pageName }) => {
   const resolved = useResolvedPath(to);
-  const isActive = useMatch({ path: resolved.pathname, end: true });
+  const isActive = useMatch({ path: resolved.pathname });
+
   return (
     <li className={`item ${isActive ? "active" : ""}`}>
-      <NavLink to={to}>{pageName}</NavLink>
+      <NavLink to={to} exact={true} activeClassName="active">
+        {pageName}
+      </NavLink>
     </li>
   );
 };
@@ -14,13 +18,13 @@ const CustomLink = ({ to, pageName }) => {
 const Navbar = () => {
   return (
     <nav className="app__navbar">
-      <NavLink className="app__home" to="/">
+      <NavLink className="app__home" to="/" exact activeClassName="active">
         Home
       </NavLink>
       <ul className="nav__list">
-        <CustomLink to="/newrecipe" pageName="New Recipe" />
-        <CustomLink to="/signin" pageName="Sign-in" />
-        <CustomLink to="/signup" pageName="Sign-up" />
+        <CustomLink to="/new-recipe" pageName="New Recipe" />
+        <CustomLink to="/sign-in" pageName="Sign-in" />
+        <CustomLink to="/sign-up" pageName="Sign-up" />
         <CustomLink to="/settings" pageName="User Settings" />
       </ul>
     </nav>
