44f17e4183e78c304110f01ac83c63156d3a71d8
Savas Pamuk <142830162+savaspamuk@users.noreply.github.com>
2023-12-28 15:24:28 +0100

    SAVAS - Lesson 35 (#140)
    * SAVAS-Navbar was created
    
    * SAVAS-Some corrections based on suggestions
    
    * SAVAS-Some corrections and stylings
    
    * fix: fixing some code errors on routing
    
    * refactor(navbar): optimize code

--- JS/Lesson35/savas/my-app/src/components/RecipeDetail/index.jsx
+++ JS/Lesson35/savas/my-app/src/components/RecipeDetail/index.jsx
@@ -1,12 +1,22 @@
-import * as React from 'react';
-import { useParams } from 'react-router-dom';
-
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export const RecipeDetail = () => {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState();
 
-    let { id } = useParams();
-  
-    return(
-        <div>recipe detail: {id}</div>
-    )
+  useEffect(() => {
+    if (id) {
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then((response) => response.json())
+        .then((data) => setRecipe(data.meals[0]));
+    }
+  }, [id]);
+
+  return recipe ? (
+    <div>Recipe Detail: {recipe.strMeal}</div>
+  ) : (
+    <div>Loading...</div>
+  );
 };
