968c24b250e3c64ee7f953942051472cbd2153c7
@TugbaMartin <tugba.martin01@gmail.com>
2024-01-15 11:52:23 +0300

    lesson34-newRecipe

--- JS/Lesson34/tugba/my-app/src/components/NewRecipe/index.jsx
+++ JS/Lesson34/tugba/my-app/src/components/NewRecipe/index.jsx
@@ -1,77 +1,99 @@
 import { useForm } from 'react-hook-form';
 import './styles.css';
 
-// const useMyHook = () => {
-//   const myData = {id: 'xxxx', name:'Data name'};
-//   const deleteMyData = () => {console.log('Deleting data...')};
-
-//   return [ myData, deleteMyData ];
-// }
-
 export const NewRecipe = () => {
-  // const [data,  setData] = useMyHook();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      strMeal: '',
+      strDrinkAlternate: '',
+      strCategory: '',
+      strCategoryOther: '',
+      strInstructions: '', 
+      strIngredientsAndMeasurements: '',
+    },
+  });
 
-  // console.log(data);
-  // setData();
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: { errors },
-	} = useForm();
+  const strCategory = watch('strCategory');
 
-	const strCategory = watch('strCategory');
+  const onSubmit = (data) => {
+   
+    const ingredientsAndMeasurements = data.strIngredientsAndMeasurements.split(',').map(item => item.trim());
 
-	const onSubmit = (data) => console.log(data);
+    
+    console.log({
+      ...data,
+      ingredientsAndMeasurements,
+    });
+  };
 
-	return (
-		<form
-			className='new-recipe-form'
-			onSubmit={handleSubmit(onSubmit)}
-			noValidate
-		>
-			<label htmlFor='strMeal'>Meal name</label>
-			<input
-				required
-				id='strMeal'
-				type='text'
-				{...register('strMeal', { required: true })}
-				placeholder='Please enter meal type...'
-			/>
-			{errors.strMeal && (
-				<span className='input-error'>Meal name is required</span>
-			)}
+  return (
+    <form
+      className='new-recipe-form'
+      onSubmit={handleSubmit(onSubmit)}
+      noValidate
+    >
+      <label htmlFor='strMeal'>Meal name</label>
+      <input
+        id='strMeal'
+        type='text'
+        {...register('strMeal', { required: true })}
+        placeholder='Please enter meal type...'
+      />
+      {errors.strMeal && (
+        <span className='input-error'>Meal name is required</span>
+      )}
 
-			<label htmlFor='strDrinkAlternate'>Drink alternate</label>
-			<input
-				id='strDrinkAlternate'
-				type='text'
-				{...register('strDrinkAlternate')}
-			/>
+      <label htmlFor='strDrinkAlternate'>Drink alternate</label>
+      <input
+        id='strDrinkAlternate'
+        type='text'
+        {...register('strDrinkAlternate')}
+      />
 
-			<label htmlFor='strCategory'> Meal category</label>
-			<select
-				id='strCategory'
-				{...register('strCategory', { required: true, minLength: 1 })}
-			>
-				<option value=''>Select category</option>
-				<option value='Side'>Side</option>
-				<option value='Main'>Main</option>
-				<option value='Dessert'>Dessert</option>
-				<option value='Other'>Other</option>
-			</select>
-			{errors.strCategory && (
-				<span className='input-error'>Category is required</span>
-			)}
-			{strCategory === 'Other' && (
-				<input
-					type='text'
-					{...register('strCategoryOther', { required: true, minLength: 3 })}
-					placeholder='Please enter category...'
-				/>
-			)}
+      <label htmlFor='strCategory'>Meal category</label>
+      <select
+        id='strCategory'
+        {...register('strCategory', { required: true, minLength: 1 })}
+      >
+        <option value=''>Select category</option>
+        <option value='Side'>Side</option>
+        <option value='Main'>Main</option>
+        <option value='Dessert'>Dessert</option>
+        <option value='Other'>Other</option>
+      </select>
+      {errors.strCategory && (
+        <span className='input-error'>Category is required</span>
+      )}
+      {strCategory === 'Other' && (
+        <input
+          type='text'
+          {...register('strCategoryOther', { required: true, minLength: 3 })}
+          placeholder='Please enter category...'
+        />
+      )}
 
-			<button type='Submit'>Submit</button>
-		</form>
-	);
+      
+      <label htmlFor='strInstructions'>Instructions</label>
+      <textarea
+        id='strInstructions'
+        {...register('strInstructions')}
+        placeholder='Enter cooking instructions...'
+      />
+
+      <label htmlFor='strIngredientsAndMeasurements'>Ingredients and Measurements</label>
+      <textarea
+        id='strIngredientsAndMeasurements'
+        {...register('strIngredientsAndMeasurements')}
+        placeholder='e.g., Flour - 1dl, milk - 2dl, butter - 100g'
+      />
+
+      <button type='submit'>Submit</button>
+    </form>
+  );
 };
+
