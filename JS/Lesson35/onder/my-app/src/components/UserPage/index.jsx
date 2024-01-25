09cce83b0a3c3543b62e77d2f18c25f13b5bbee4
ondermartin <ondermartin@gmail.com>
2024-01-25 20:00:06 +0300

    OnderLesson35-36-JS

--- JS/Lesson35/onder/my-app/src/components/UserPage/index.jsx
+++ JS/Lesson35/onder/my-app/src/components/UserPage/index.jsx
@@ -1,3 +1,19 @@
-export const UserPage = () => {
-  return (<div>User page</div>)
+import React from "react";
+import { Typography, Paper, Container } from "@mui/material";
+
+const UserPage = () => {
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3} style={{ padding: "20px", marginTop: "50px" }}>
+        <Typography variant="h5" gutterBottom>
+          User Page
+        </Typography>
+        <Typography>
+          Welcome to your user page. Customize your settings here.
+        </Typography>
+      </Paper>
+    </Container>
+  );
 };
+
+export default UserPage;
